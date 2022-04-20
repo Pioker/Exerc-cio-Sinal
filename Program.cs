f677134fc9da966aa07688c2e9f08f7502146601
Giseli Gomes Pioker <gipioker@hotmail.com>
2022-04-20 09:50:20 -0300

    resoluçao exercicio Sinal

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿int numero;
+
+Console.Write("Digite um número: ");
+numero = Convert.ToInt32(Console.ReadLine());
+if (numero < 0)
+{
+Console.WriteLine("Negativo");
+}
+else if (numero > 0)
+{
+Console.WriteLine("Positivo");
+}
+else
+{
+Console.WriteLine("Zero");
+}
